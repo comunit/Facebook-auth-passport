9572a2d744037d6e99eea1ece2323ed35ae83dcd
comunit <comunit76@gmail.com>
2017-11-06 17:49:51 +0000

    made some changes

---
+++ config/keys.js
@@ -0,0 +1,14 @@
+module.exports = {
+  facebook: {
+    'appID' : '1958653771070440',
+    'appSecret' : '5d899db2a85b2975d549846acd5364f7'
+  },
+
+  mongodb: {
+    'dbURI' : 'mongodb://comunit:imran123@imran-shard-00-00-drfa7.mongodb.net:27017,imran-shard-00-01-drfa7.mongodb.net:27017,imran-shard-00-02-drfa7.mongodb.net:27017/test?ssl=true&replicaSet=Imran-shard-0&authSource=admin'
+  },
+
+  session: {
+    cookieKey: 'jdshfuiehfuiwhfuw'
+  }
+};
